669d72d76e32cbbc24b133d2fdae0801ca85b064
Yunus Kılıç <yunskilic@gmail.com>
2018-08-11 16:13:59 +0300

    problem link added

--- 2DArrayDS.java
+++ 2DArrayDS.java
@@ -1,3 +1,6 @@
+//Problem
+//https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
